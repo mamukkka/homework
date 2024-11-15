8952caeacb830796264205c4db11c1ca2ff9f8e4
mamukkka <itisfortesting2023@gmail.com>
2024-11-15 10:50:21 -0500

    Comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("რომელი ქალაქია საქართველოს დედაქალაქი ?");
-        String correctAnswer = "თბილისი";
-        String answer = scanner.nextLine();
 
-        while (!answer.equals(correctAnswer)){
+        Scanner scanner = new Scanner(System.in); // სკანერის ობიექტის შექმნა, მომხმარებლის პასუხის გასაგებად
+        System.out.println("რომელი ქალაქია საქართველოს დედაქალაქი ?"); // მომხმარებლისთვის დასასმელი კითხვა
+        String correctAnswer = "თბილისი"; // სწორი პასუხი
+        String answer = scanner.nextLine(); // მომხმარებლის პასუხი
+
+        while (!answer.equals(correctAnswer)){ // ციკლი, რომელიც ამეორებს კითხვას თუ პასუხი არასწორია
             System.out.println("პასუხი არასწორია, კიდევ სცადეთ..");
             answer = scanner.nextLine();
         }
 
-        System.out.println("პასუხი სწორია, გილოცავთ თქვენ გეოგრაფიის გურუ ხართ.");
+        System.out.println("პასუხი სწორია, გილოცავთ თქვენ გეოგრაფიის გურუ ხართ."); // შეტყობინება თუ მომხმარებელმა სწორად უპასუხა
 
-        }
+    }
 
-        }
+}
